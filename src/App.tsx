a307c2e8295db707944b35fcdd9b41bfcb42123b
ManoelBarros22 <126240510+ManoelBarros22@users.noreply.github.com>
2023-08-14 10:23:27 -0300

    Atualização
    Atualização meus dados

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,32 @@
-import Teste from './teste'
+import { useState } from 'react'
+import { ThemeProvider } from 'styled-components'
+
+import Projetos from './containers/Projetos'
+import Sidebar from './containers/Sidebar'
+import Sobre from './containers/Sobre'
+import EstiloGlobal, { Container } from './styles'
+import temaLight from './themes/light'
+import temaDark from './themes/dark'
 
 function App() {
-  return <Teste />
+  const [estaUsandoTemaDark, setEstaUsandoTemaDark] = useState(false)
+
+  function trocaTema() {
+    setEstaUsandoTemaDark(!estaUsandoTemaDark)
+  }
+
+  return (
+    <ThemeProvider theme={estaUsandoTemaDark ? temaDark : temaLight}>
+      <EstiloGlobal />
+      <Container>
+        <Sidebar trocaTema={trocaTema} />
+        <main>
+          <Sobre />
+          <Projetos />
+        </main>
+      </Container>
+    </ThemeProvider>
+  )
 }
 
 export default App
